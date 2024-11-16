782677f33d8717ae6c6c8b035c22c87eaf0dabe7
Phanasura <trisphan15012007@gmail.com>
2024-11-16 23:24:23 +0700

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
 // service-worker.js
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installing...');
-    event.waitUntil(
-      caches.open('static-cache').then((cache) => {
-        return cache.addAll([
-          '/',                // Tệp HTML chính
-          '/index.html',      // Đường dẫn đến tệp HTML
-          '/minicon.png',// Biểu tượng 192px
-          '/maxicon.jpg' // Biểu tượng 512px
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    console.log('Fetching:', event.request.url);
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  console.log('Service Worker installing...');
+  event.waitUntil(
+    caches.open('static-cache').then((cache) => {
+      return cache.addAll([
+        './',                 // Đường dẫn gốc (index.html)
+        './index.html',       // Tệp HTML
+        './script.js',        // Tệp JavaScript chính
+        './manifest.json',    // Tệp Manifest
+        './icon-192x192.png', // Biểu tượng 192px
+        './icon-512x512.png'  // Biểu tượng 512px
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Fetching:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
